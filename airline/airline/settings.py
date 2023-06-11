080cdab0d4ff9a57f3d737c64b388cb3bc8e052b
Jossue Jativa <jativatamayojossuedavid@gmail.com>
2023-06-11 12:24:55 -0500

    Creacion de la carpeta migrations con el comando python manage.py makemigrations

--- airline/airline/settings.py
+++ airline/airline/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'flight'
+    'flight',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
